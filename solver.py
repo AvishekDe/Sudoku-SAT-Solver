d50c5bc4395b1dbea40b00483197cb65f71f5471
Avishek De <avishekde.iitr@gmail.com>
2022-05-21 15:46:45 -0700

    making solvers configurable

--- solver.py
+++ solver.py
@@ -1,6 +1,6 @@
 import itertools
 import os
-
+import sys
 
 puzzle = [
     "5***1***4",
@@ -26,6 +26,89 @@ def exactly_one_of(literals):
 
     for pair in itertools.combinations(literals,2):
         clauses.append([-l for l in pair])
+
+def invoke_solver(name):
+    if(name == "minisat"):
+        os.system("minisat tmp.cnf minisat.sat > minisat_stats.out")
+    elif(name == "z3"):
+        os.system("z3 tmp.cnf > z3.sat")
+    elif(name == "sat4j"):
+        os.system("java -jar org.sat4j.core-2.3.1.jar tmp.cnf > sat4j.sat")
+    elif(name == "all"):
+        os.system("minisat tmp.cnf minisat.sat > minisat_stats.out")
+        os.system("z3 tmp.cnf > z3.sat")
+        os.system("java -jar org.sat4j.core-2.3.1.jar tmp.cnf > sat4j.sat")
+
+def print_minisat():
+    print("-------MINISAT Solver-------")
+    with open("minisat.sat", "r") as satfile:
+        for line in satfile:
+            if line.split()[0] == "UNSAT":
+                print("UNSAT")
+            elif line.split()[0] == "SAT":
+                print("SAT")
+            else:
+                assignment = [int(x) for x in line.split()]
+
+                for i in digits:
+                    for j in digits:
+                        for k in digits:
+                            if(varnum(i,j,k) in assignment):
+                                print(k, end="")
+                                break
+                    print("")
+
+def print_z3():
+    print("-------Z3 Solver-------")
+    with open("z3.sat", "r") as satfile:
+        for line in satfile:
+            if line.split()[0] == "s" and line.split()[1] == "UNSATISFIABLE":
+                print("UNSAT")
+            elif line.split()[0] == "s" and line.split()[1] == "SATISFIABLE":
+                print("SAT")
+            else:
+                assignment = [int(x) for x in line.split()[1:]]
+
+                for i in digits:
+                    for j in digits:
+                        for k in digits:
+                            if(varnum(i,j,k) in assignment):
+                                print(k, end="")
+                                break
+                    print("")
+
+def print_sat4j():
+    print("-------SAT4J Solver-------")
+    with open("sat4j.sat", "r") as satfile:
+        for line in satfile:
+            if(line.split()[0] == "c"):
+                pass
+            elif line.split()[0] == "s" and line.split()[1] == "UNSATISFIABLE":
+                print("UNSAT")
+            elif line.split()[0] == "s" and line.split()[1] == "SATISFIABLE":
+                print("SAT")
+            else:
+                assignment = [int(x) for x in line.split()[1:]]
+
+                for i in digits:
+                    for j in digits:
+                        for k in digits:
+                            if(varnum(i,j,k) in assignment):
+                                print(k, end="")
+                                break
+                    print("")
+
+def print_results(name):
+    if(name == "minisat"):
+        print_minisat()
+    elif(name == "z3"):
+        print_z3()
+    elif(name == "sat4j"):
+        print_sat4j()
+    elif(name == "all"):
+        print_minisat()
+        print_z3()
+        print_sat4j()
 
 # cell (i,j) contains exactly 1 digit
 for(i,j) in itertools.product(digits, repeat=2):
@@ -57,21 +140,9 @@ with open("tmp.cnf", "w") as f:
         c.append(0);
         f.write(" ".join(map(str,c))+"\n")
 
-os.system("minisat tmp.cnf tmp.sat")
 
-with open("tmp.sat", "r") as satfile:
-    for line in satfile:
-        if line.split()[0] == "UNSAT":
-            print("There is no solution")
-        elif line.split()[0] == "SAT":
-            pass
-        else:
-            assignment = [int(x) for x in line.split()]
+# check which solver we are using
+solver_name_input = sys.argv[1]
 
-            for i in digits:
-                for j in digits:
-                    for k in digits:
-                        if(varnum(i,j,k) in assignment):
-                            print(k, end="")
-                            break
-                print("")
+invoke_solver(solver_name_input)
+print_results(solver_name_input)

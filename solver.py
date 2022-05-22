15f24c96552913ae567a899f6bdba7ff9e09354a
Ajit Jadhav <ajit.171198@gmail.com>
2022-05-22 15:24:09 -0700

    Add Sudoku hint mechanism

--- solver.py
+++ solver.py
@@ -3,22 +3,39 @@ import os
 import sys
 import time
 
-puzzle = [
-    "5***1***4",
-    "***2*6***",
-    "**8*9*6**",
-    "*4*****1*",
-    "7*1*8*4*6",
-    "*5*****3*",
-    "**6*4*1**",
-    "***5*2***",
-    "2***6***8"
-]
 
-timings = {}
+def load_puzzle(solution_filename, mode):
+    solution = []
 
-clauses = []
-digits = range(1,10)
+    if mode == 'test':
+        puzzle_filename = solution_filename
+    else:
+        solution_filepath = os.path.join('Solutions', solution_filename)
+        with open(solution_filepath) as f:
+            for line in f:
+                solution.append(line.strip())
+        puzzle_filename = solution[0]
+        solution = solution[1:]
+
+    puzzle_filepath = os.path.join('Puzzles', puzzle_filename)
+    puzzle = []
+    with open(puzzle_filepath, 'r') as f:
+        for line in f:
+            puzzle.append(line)
+    return puzzle, solution  
+
+def compare_solution(solution, answer):
+    for i in range(len(solution)):
+        if i%3 == 0 and i != 0:
+            print('_'*44)
+        for j in range(len(solution[0])):
+            if j%3 == 0 and j != 0:
+                print('| ', end='')
+            if solution[i][j].isdigit() and solution[i][j] != answer[i][j]:
+                print(f'{solution[i][j]}->{answer[i][j]}', end=' ')
+            else:
+                print(f'{solution[i][j]}   ', end=' ')
+        print()
 
 def varnum(i,j,k):
     assert(i in digits and j in digits and k in digits)
@@ -118,40 +135,79 @@ def print_results(name):
         print_z3()
         print_sat4j()
 
-# cell (i,j) contains exactly 1 digit
-for(i,j) in itertools.product(digits, repeat=2):
-    exactly_one_of([varnum(i,j,k) for k in digits])
+if __name__ == '__main__':
+    timings = {}
 
-# k appears exactly once in row i
-for(i,k) in itertools.product(digits, repeat=2):
-    exactly_one_of([varnum(i,j,k) for j in digits])
+    if(len(sys.argv[1:]) < 3):
+        print('Argument error, not enough arguments provided')
+        exit()
 
-# k appears exactly once in column j
-for(j,k) in itertools.product(digits, repeat=2):
-    exactly_one_of([varnum(i,j,k) for i in digits])
+    mode = sys.argv[1]
 
-# k appears exactly once in mini-grid
-for(i,j) in itertools.product([1,4,7], repeat=2):
-    for k in digits:
-        exactly_one_of([varnum(i+deltai,j+deltaj,k) for (deltai,deltaj) in itertools.product(range(3), repeat=2)])
+    # check which solver we are using
+    solver_name_input = sys.argv[2]
+    puzzle, solution = load_puzzle(sys.argv[3], mode)
 
-for(i,j) in itertools.product(digits, repeat=2):
-    if puzzle[i-1][j-1]!="*":
-        k = int(puzzle[i-1][j-1])
-        assert(k in digits)
-        # i,j already contains k
-        clauses.append([varnum(i,j,k)])
+    clauses = []
+    digits = range(1,10)
 
-with open("tmp.cnf", "w") as f:
-    f.write("p cnf {} {}\n".format(999, len(clauses)))
-    for c in clauses:
-        c.append(0);
-        f.write(" ".join(map(str,c))+"\n")
+    # cell (i,j) contains exactly 1 digit
+    for(i,j) in itertools.product(digits, repeat=2):
+        exactly_one_of([varnum(i,j,k) for k in digits])
 
+    # k appears exactly once in row i
+    for(i,k) in itertools.product(digits, repeat=2):
+        exactly_one_of([varnum(i,j,k) for j in digits])
 
-# check which solver we are using
-solver_name_input = sys.argv[1]
+    # k appears exactly once in column j
+    for(j,k) in itertools.product(digits, repeat=2):
+        exactly_one_of([varnum(i,j,k) for i in digits])
 
-invoke_solver(solver_name_input)
-print_results(solver_name_input)
-print(timings)
+    # k appears exactly once in mini-grid
+    for(i,j) in itertools.product([1,4,7], repeat=2):
+        for k in digits:
+            exactly_one_of([varnum(i+deltai,j+deltaj,k) for (deltai,deltaj) in itertools.product(range(3), repeat=2)])
+
+    for(i,j) in itertools.product(digits, repeat=2):
+        if puzzle[i-1][j-1]!="*":
+            k = int(puzzle[i-1][j-1])
+            assert(k in digits)
+            # i,j already contains k
+            clauses.append([varnum(i,j,k)])
+
+    with open("tmp.cnf", "w") as f:
+        f.write("p cnf {} {}\n".format(999, len(clauses)))
+        for c in clauses:
+            c.append(0);
+            f.write(" ".join(map(str,c))+"\n")
+
+    if mode == 'test':
+        invoke_solver(solver_name_input)
+        print_results(solver_name_input)
+        print(timings)
+    
+    elif mode == 'solve':
+        start = time.time()
+        os.system("minisat tmp.cnf tmp.sat")
+        end = time.time()
+        print ("Time elapsed:", end - start)
+        
+        with open("tmp.sat", "r") as satfile:
+            for line in satfile:
+                if line.split()[0] == "UNSAT":
+                    print("There is no solution")
+                elif line.split()[0] == "SAT":
+                    pass
+                else:
+                    assignment = [int(x) for x in line.split()]
+                    answer = []
+                    for i in digits:
+                        line = ''
+                        for j in digits:
+                            for k in digits:
+                                if(varnum(i,j,k) in assignment):
+                                    line += str(k)
+                                    break
+                        answer.append(line)
+                    compare_solution(solution, answer)
+
